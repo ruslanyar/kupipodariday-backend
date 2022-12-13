3f43570fc387f1f62d639858767ade0a7f536302
ruslanyar <styxrus@yandex.ru>
2022-12-14 00:20:39 +0300

    build: add db connection and TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
+import configuration from '../configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...configuration,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
