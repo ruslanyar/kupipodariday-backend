71eb61baf2ed57bd26937a7889c9a9e3ca52de60
ruslanyar <styxrus@yandex.ru>
2022-12-24 22:54:20 +0300

    feat: set jwt token expiresIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,7 +16,9 @@ export class AuthService {
   auth(user: User) {
     const payload = { sub: user.id };
 
-    return { access_token: this.jwtService.sign(payload) };
+    return {
+      access_token: this.jwtService.sign(payload, { expiresIn: '24h' }),
+    };
   }
 
   async validateUser(username: string, pass: string) {
